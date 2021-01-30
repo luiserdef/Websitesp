e976613108968fa5da96f96becefc3846b0b836f
Luis Sánchez <sanchezluis9820@gmail.com>
2021-01-29 23:46:06 -0500

    sass added

--- bankLandingPage/page.js
+++
@@ -1,8 +0,0 @@
-let toogleButton = document.querySelector(".toogle-menu-nav")
-let menu = document.querySelector(".menu-nav")
-
-toogleButton.addEventListener("click", actionToogle)
-
-function actionToogle() {
-    menu.classList.toggle("hide-nav")
-}
